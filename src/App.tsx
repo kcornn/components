468a06e049ea275bb3f85747a8601588e03f9b31
Kali Cornn <kcornn@alumni.stanford.edu>
2026-01-06 11:40:21 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline hover:bg-sky-700">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
