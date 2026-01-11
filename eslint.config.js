58bac504ed7d7623d547030393b23242a38e772e
Kali Cornn <kcornn@alumni.stanford.edu>
2026-01-10 23:30:58 -0800

    run format and fix lint config

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,12 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores([
+    "dist",
+    "storybook/**",
+    "storybook-static/**",
+    "storybook/storybook-static/**",
+  ]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
