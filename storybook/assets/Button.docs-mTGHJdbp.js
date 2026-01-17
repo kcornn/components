0d1e0adbce13e62e0fc7783bec1c860073c9f957
Kali Cornn <kcornn@alumni.stanford.edu>
2026-01-16 22:41:15 -0800

    Updates

--- storybook/assets/Button.docs-mTGHJdbp.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as r}from"./index-D-sp-i4p.js";import{M as s,C as i,S as a,a as m}from"./blocks-CC1YTWcU.js";import{B as c,P as p}from"./Button.stories-Bfso08XD.js";import"./iframe-CsjVLL04.js";import"./preload-helper-DUauIqh9.js";import"./index-B4QhmGz2.js";import"./Button-CTGSFmVm.js";function e(o){const n={code:"code",h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Components/Button",of:c}),`
-`,t.jsx(n.h1,{id:"button",children:"Button"}),`
-`,t.jsxs(n.p,{children:["Use the interactive playground below to test ",t.jsx(n.code,{children:"Button"})," props."]}),`
-`,t.jsx(i,{children:t.jsx(a,{of:p})}),`
-`,t.jsx(m,{})]})}function M(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};
