37287bc96c28d854e97026fd849452fea55e3ac8
Kali Cornn <kcornn@alumni.stanford.edu>
2026-01-19 20:16:33 -0800

    Updates

---
+++ storybook/assets/Button.docs-CayQJlr5.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-u17CrQMm.js";import{useMDXComponents as r}from"./index-U5d2MbOm.js";import{M as s,C as i,S as m,a}from"./blocks-C0HPKr2n.js";import{B as c,P as p}from"./Button.stories-CEMYm8ti.js";import"./iframe-Bh5grHrG.js";import"./preload-helper-DUauIqh9.js";import"./index-Ve_na7g8.js";import"./Button-C9_AYcbh.js";import"./createSvgIcon-DFKM-E6r.js";function e(o){const n={code:"code",h1:"h1",p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Components/Button",of:c}),`
+`,t.jsx(n.h1,{id:"button",children:"Button"}),`
+`,t.jsxs(n.p,{children:["Use the interactive playground below to test ",t.jsx(n.code,{children:"Button"})," props."]}),`
+`,t.jsx(i,{children:t.jsx(m,{of:p})}),`
+`,t.jsx(a,{})]})}function B(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{B as default};
---
+++ storybook/assets/formatter-QJ4M4OGQ-l4PvbAfM.js
@@ -0,0 +1,1 @@
+import{d as m,r as o}from"./blocks-C0HPKr2n.js";import{b as t}from"./iframe-Bh5grHrG.js";import"./preload-helper-DUauIqh9.js";import"./index-Ve_na7g8.js";import"./jsx-runtime-u17CrQMm.js";var e=t(o(),1),_=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{_ as formatter};
